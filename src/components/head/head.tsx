e5eef353990f5d09a207103d71c21f96c468cc8a
Tombosoa <hei.nantenaina.4@gmail.com>
2024-12-09 17:02:28 +0300

    chore: refactor head

--- src/components/head/head.tsx
+++ src/components/head/head.tsx
@@ -1,17 +1,18 @@
 import { FlipWords } from "../ui/flip-words";
 
 const words = [
-    "web developer",
-    "programmer"
-]
+  "web developer",
+  "programmer",
+];
 
-export default function Head(){
-    return (
-        <div className="m-[2rem] flex-col">
-            <p className="text-white text-[2rem]">
-                Hi there! I'm a 
-            </p>
-            <FlipWords className="text-[3.5rem] ml-[3rem]" words={words} />
-        </div>
-    )
+export default function Head() {
+  return (
+    <div className="m-[2rem] flex flex-col">
+      <p className="text-white text-[2rem]">
+        Hi there! I'm a
+      </p>
+      {/* Vérifiez que `FlipWords` accepte bien `className` et `words` */}
+      <FlipWords className="text-[3.5rem] ml-[3rem]" words={words} />
+    </div>
+  );
 }
